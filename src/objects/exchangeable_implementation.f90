2c0f402cd1c0d9dffdecad611e11c5c63dffce34
Damian Rouson <damian@sourceryinstitute.org>
2017-03-09 11:02:47 -0800

    Exchange routine compiles but executable hangs

--- src/objects/exchangeable_implementation.f90
+++ src/objects/exchangeable_implementation.f90
@@ -2,6 +2,8 @@ submodule(exchangeable_interface) exchangeable_implementation
   implicit none
 
   integer, parameter :: default_halo_size=5
+  integer, save, allocatable :: neighbors(:)
+  integer, save :: north_neighbor, south_neighbor, halo_size
 
 contains
 
@@ -10,25 +12,69 @@ contains
     integer, intent(in) :: grid_dims(:)
     real, intent(in) :: initial_value
     integer, intent(in), optional :: halo_width
-    integer :: halo_size
+
     if (present(halo_width)) then
         halo_size = halo_width
     else
         halo_size = default_halo_size 
     end if
+
     if (allocated(this%local)) deallocate(this%local)
     this%north_boundary = (this_image() == num_images())
     this%south_boundary = (this_image() == 1)
+
     associate( local_halo_size=>2*halo_size - merge(halo_size,0,this%south_boundary) - merge(halo_size,0,this%north_boundary) ) 
       allocate(this%local(grid_dims(1),grid_dims(2),grid_dims(3) + local_halo_size),source=initial_value)
     end associate
+
     allocate( this%halo_south_in( grid_dims(1),grid_dims(2),halo_size)[*], source=initial_value)
     allocate( this%halo_north_in( grid_dims(1),grid_dims(2),halo_size)[*], source=initial_value)
+
+    associate(me=>this_image())
+      south_neighbor = me-1
+      north_neighbor = me+1
+      if (this%north_boundary .and. this%south_boundary) then
+        neighbors = [me]
+      else if (this%south_boundary) then
+        neighbors = [north_neighbor]
+      else if (this%north_boundary) then
+        neighbors = [south_neighbor]
+      else ! internal cells
+        neighbors = [me-1,me+1]
+      end if
+    end associate
+    
   end subroutine
 
   module subroutine exchange(this)
     class(exchangeable_t), intent(inout) :: this
-    print *,"exchange unimplemented"
+    if (.not. this%north_boundary) call put_north
+    if (.not. this%south_boundary) call put_south
+    sync images( neighbors )
+    if (.not. this%north_boundary) call retrieve_north_halo
+    if (.not. this%south_boundary) call retrieve_south_halo
+  contains
+
+    subroutine put_north
+      integer :: n
+      n = size(this%local,3)
+      this%halo_south_in(:,:,:)[north_neighbor] = this%local(:,:,n-halo_size:n)
+    end subroutine
+
+    subroutine put_south
+      this%halo_north_in(:,:,:)[south_neighbor] = this%local(:,:,1:halo_size)
+    end subroutine
+
+    subroutine retrieve_north_halo
+      integer :: n
+      n = size(this%local,3)
+      this%local(:,:,n-halo_size:n) = this%halo_north_in
+    end subroutine
+
+    subroutine retrieve_south_halo
+      this%local(:,:,1:halo_size) = this%halo_south_in
+    end subroutine
+
   end subroutine
 
 end submodule
--- src/tests/test-initialization.f90
+++ src/tests/test-initialization.f90
@@ -4,6 +4,8 @@ program main
   use assertions_interface, only : assert
   implicit none
 
+  if (this_image()==1) print *,"Number of images = ",num_images()
+
   block
     type(domain_t) :: domain
     print *,"domain%default_initialize()"
@@ -14,13 +16,13 @@ program main
     type(domain_t) :: domain
     print *,"domain%initialize_from_file('input-parameters.txt')"
     call domain%initialize_from_file('input-parameters.txt')
+  
+   !print *,"domain%advect(dt = 4.0)"
+   !call domain%advect(dt = 4.0)
+  
+    print *,"domain%halo_exchange()"
+    call domain%halo_exchange()
   end block
-  
- !print *,"domain%advect(dt = 4.0)"
- !call domain%advect(dt = 4.0)
-  
- !print *,"domain%halo_exchange()"
- !call domain%halo_exchange()
 
  print *,"Test passed."
 end program
