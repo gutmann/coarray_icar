e44d4bc9d07ccaec9d844855f765bbc45f67e517
Ethan Gutmann <gutmann@ucar.edu>
2017-06-15 18:16:44 -0600

    updates to mp_driver code

--- src/physics/mp_driver.f90
+++ src/physics/mp_driver.f90
@@ -1,15 +1,25 @@
-module mp_driver
+module module_mp_driver
 
+    use domain_interface
     use module_mp_thompson
 
     logical :: initialized
 contains
 
+    subroutine mp_init(domain)
+        implicit none
+        type(domain_t), intent(inout) :: domain
+
+        call thompson_init()
+
+    end subroutine mp_init
+
     subroutine microphysics(domain)
         implicit none
         type(domain_t), intent(inout) :: domain
 
-
+        if (.not.initialized) call mp_init(domain)
 
     end subroutine microphysics
-end module mp_driver
+
+end module module_mp_driver
