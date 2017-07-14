bd604b73711426c33125c25d2846bc44502438bf
Ethan Gutmann <gutmann@ucar.edu>
2017-07-13 22:37:51 -0600

    created separate test-ideal and test-initialization

--- src/tests/test-ideal.f90
+++ src/tests/test-ideal.f90
@@ -6,8 +6,6 @@ program main
   implicit none
 
   if (this_image()==1) print *,"Number of images = ",num_images()
-  sync all
-  print *, "Hello from image: ", this_image()
 
 
   block
@@ -27,8 +25,6 @@ program main
     integer :: i,nz, ypos
     real :: start, finish
 
-    call cpu_time(start)
-
     if (this_image()==1) print *,this_image(),"domain%initialize_from_file('input-parameters.txt')"
     call domain%initialize_from_file('input-parameters.txt')
 
@@ -41,16 +37,25 @@ program main
         end do
     endif
 
-    if (this_image()==1) print *,"Microphysics"
-    ! note should this be wrapped into the domain object(?)
-    call microphysics(domain, dt = 20.0)
+    ypos = (ubound(domain%accumulated_precipitation,2)-lbound(domain%accumulated_precipitation,2))/2
+    ypos = ypos + lbound(domain%accumulated_precipitation,2)
+    sync all
+    call cpu_time(start)
+    do i=1,200
+        ! print *,"Microphysics"
+        ! note should this be wrapped into the domain object(?)
+        call microphysics(domain, dt = 20.0)
+        ! print *,"domain%advect(dt = 4.0)"
+        call domain%advect(dt = 1.0)
 
-    if (this_image()==1) print *,"domain%advect(dt = 4.0)"
-    call domain%advect(dt = 1.0)
+        ! print *,"domain%halo_exchange()"
+        call domain%halo_exchange()
 
-    if (this_image()==1) print *,"domain%halo_exchange()"
-    call domain%halo_exchange()
-
+        ! if (this_image()==(num_images()/2)) then
+        !     print*, this_image(), i, domain%accumulated_precipitation(::10,ypos)
+        ! endif
+        ! call domain%enforce_limits()
+    end do
     sync all
     call cpu_time(finish)
 
